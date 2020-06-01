01f7b84653c842424815c5a6a3f0cff5b8d6fae0
moutan <moutan@localhost.localdomain>
2020-06-01 11:23:25 +0000

    ready self_sup and sup finetuning

--- pytorch/finetune.py
+++ pytorch/finetune.py
@@ -1,115 +1,210 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+import sys
+import os
+import numpy as np
 
 from unet3d import UNet3D
 from dataset import Dataset
 from finetune_config import FineTuneConfig
 from stats import Statistics
 
-config = FineTuneConfig()
-stats = Statistics(config.epochs, config.batch_size, save_directory=config.stats_dir)
-dataset = Dataset(data_dir=config.data_dir)
+from dice_loss import DiceLoss
+from image_transformations import generate_pair
 
-stats.initial_lr = config.lr
-stats.tr_val_ts_split = dataset.tr_val_ts_split
-stats.task = config.finetune_task
 
-# prepare the 3D model
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = UNet3D()
-
-#Load pre-trained weights
-weight_dir = config.weights
-checkpoint = torch.load(weight_dir, map_location=device)
-state_dict = checkpoint['state_dict']
-unParalled_state_dict = {}
-for key in state_dict.keys():
-    unParalled_state_dict[key.replace("module.", "")] = state_dict[key]
-model.load_state_dict(unParalled_state_dict)
-
-model.to(device)
-model = nn.DataParallel(model, device_ids = [i for i in range(torch.cuda.device_count())])
-
-criterion = nn.MSELoss()
-
-if config.optimizer == "sgd":
-	optimizer = torch.optim.SGD(model.parameters(), config.lr, momentum=0.9, weight_decay=0.0, nesterov=False)
-elif config.optimizer == "adam":
-	optimizer = torch.optim.Adam(model.parameters(), config.lr)
-else:
-	raise Exception
-
-#lambda_lr = lambda epoch: LR_DECAY**epoch
-#scheduler_actor = torch.optim.lr_scheduler.LambdaLR(optimizer_actor, lr_lambda=lambda_lr)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=int(config.patience * 0.8), gamma=0.5)
-
-# train the model
-if config.self_supervised:
-    raise NotImplementedError
-
-best_loss = 10000000000
-num_epoch_no_improvement = 0
-
-for epoch in range(config.nb_epoch):
-    stats.training_losses = [] 
-    stats.validation_losses = []
-    scheduler.step(epoch)
-    model.train()
-    x, iteration = 0 , 0
-    while x != None: # go through all examples
-        x ,y = dataset.get_train(config.batch_size)
-        x, y = x.float().to(device), y.float().to(device)
-        pred = model(x)
-        loss = criterion(pred, y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        stats.training_losses.append(loss.item())
-        training_losses_epoch_specific.append(loss.item())
-
-        if (iteration + 1) % 5 == 0:
-            print('Epoch [{}/{}], iteration {}, Loss: {:.6f}'.format(epoch + 1, config.nb_epoch, iteration + 1, np.average(stats.training_losses_epoch_specific)))
-            sys.stdout.flush()
-        iteration += 1
-
-    with torch.no_grad():
-        model.eval()
-        x = 0
-        while x != None:
-            x, y = dataset.get_val(conf.batch_size)
-            x, y = x.float().to(device), y.float().to(device)
-            pred = model(x)
-            loss = criterion(pred,y)
-            stats.validation_losses.append(loss.item())
-            training_losses_epoch_specific.append(loss.item())
-        
-	#logging
-    stats.avg_training_loss_per_epoch.append(np.average(stats.training_losses))
-    stats.avg_validation_loss_per_epoch.append(np.average(stats.validation_losses))
-    stats.iterations.append(iteration)
+class FineTuner():
     
-    avg_training_loss_of_epoch = np.average(stats.training_losses)
+    def __init__(self,config: FineTuneConfig, dataset:Dataset, stats: Statistics):
+        
+        self.dataset = dataset
+        self.config = config
+        self.stats = stats
+        self._loadmodel()
 
-	print("Epoch {}, validation loss is {:.4f}, training loss is {:.4f}".format(epoch+1,valid_loss,train_loss))
-	if avg_validation_loss_of_epoch < best_loss:
-		print("Validation loss decreases from {:.4f} to {:.4f}".format(best_loss, valid_loss))
-		best_loss = avg_validation_loss_of_epoch
-		num_epoch_no_improvement = 0
-		#save model
-		torch.save({
-			'epoch': epoch+1,
-			'state_dict' : model.state_dict(),
-			'optimizer_state_dict': optimizer.state_dict()
-		},os.path.join(conf.model_path_save, conf.finetune_task + ".pt"))
-		print("Saving model ",os.path.join(conf.model_path_save, conf.finetune_task + ".pt"))
-	else:
-		print("Validation loss did not decrease from {:.4f}, num_epoch_no_improvement {}".format(best_loss,num_epoch_no_improvement))
-		num_epoch_no_improvement += 1
-	if num_epoch_no_improvement >= conf.patience:
-		print("Early Stopping")
-		stats.stopped_early = True
-		break
-	sys.stdout.flush()
+    def finetune_self_supervised (self):
+        
+        if self.config.optimizer_ss == "sgd":
+            optimizer = torch.optim.SGD(self.model.parameters(), self.config.lr_ss, momentum=0.9, weight_decay=0.0, nesterov=False)
+        elif self.config.optimizer_ss == "adam":
+            optimizer = torch.optim.Adam(self.model.parameters(), self.config.lr_ss)
+        
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=int(self.config.patience_ss * 0.8), gamma=0.5)
 
-stats.get_statistics() 
+        best_loss = 10000000000
+        num_epoch_no_improvement = 0
+        criterion = nn.MSELoss() 
+
+        for epoch in range(self.config.nb_epoch_ss):
+            stats.training_losses_ss = [] 
+            stats.validation_losses_ss = []
+            scheduler.step(epoch)
+            self.model.train()
+            x, iteration = 0 , 0
+            while x != None: # go through all examples
+                x, _ = self.dataset.get_train(self.config.batch_size_ss, return_tensor=False)
+                x_transform , y = generate_pair(x, self.config.batch_size_ss, self.config, return_pair=True, make_tensors=True) 
+                x_transform, y = x_transform.float().to(self.device), y.float().to(self.device)
+                pred = self.model(x_transform)
+                loss = criterion(pred, y)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                stats.training_losses_ss.append(loss.item())
+
+                if (iteration + 1) % 5 == 0:
+                    print('Epoch [{}/{}], iteration {}, Loss: {:.6f}'.format(epoch + 1, self.config.nb_epoch_ss, iteration + 1, np.average(stats.training_losses_ss)))
+                    sys.stdout.flush()
+                iteration += 1
+
+            with torch.no_grad():
+                self.model.eval()
+                x = 0
+                while x != None:
+                    x, _ = self.dataset.get_val(self.config.batch_size_ss, return_tensor=False)
+                    x_transform , y = generate_pair(x,self.config.batch_size_ss, self.config, return_pair=True, make_tensors=True) 
+                    x_transform, y = x_transform.float().to(self.device), y.float().to(self.device)
+                    pred = self.model(x_transform)
+                    loss = criterion(pred,y)
+                    stats.validation_losses_ss.append(loss.item())
+                    
+            avg_training_loss_of_epoch = np.average(self.stats.training_losses_ss)      
+            avg_validation_loss_of_epoch = np.average(self.stats.validation_losses_ss)            
+            self.stats.avg_training_loss_per_epoch_ss.append(avg_training_loss_of_epoch)
+            self.stats.avg_validation_loss_per_epoch_ss.append(avg_validation_loss_of_epoch)
+            self.stats.iterations_ss.append(iteration)
+        
+            print("###### SELF SUPERVISED#######")
+            print("Epoch {}, validation loss is {:.4f}, training loss is {:.4f}".format(epoch+1,avg_validation_loss_of_epoch,avg_training_loss_of_epoch))
+            if avg_validation_loss_of_epoch < best_loss:
+                print("Validation loss decreases from {:.4f} to {:.4f}".format(best_loss, avg_validation_loss_of_epoch))
+                best_loss = avg_validation_loss_of_epoch
+                num_epoch_no_improvement = 0
+                #save model
+                torch.save({
+                    'epoch': epoch+1,
+                    'state_dict' : self.model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict()
+                },os.path.join(self.config.model_path_save, self.config.finetune_task + ".pt"))
+                print("Saving model ",os.path.join(self.config.model_path_save, self.config.finetune_task + ".pt"))
+            else:
+                print("Validation loss did not decrease from {:.4f}, num_epoch_no_improvement {}".format(best_loss,num_epoch_no_improvement))
+                num_epoch_no_improvement += 1
+            if num_epoch_no_improvement >= self.config.patience_ss:
+                print("Early Stopping")
+                self.stats.stopped_early = True
+                break
+            sys.stdout.flush()
+            
+        self.stats.get_statistics() 
+        
+    def finetune_supervised(self, config: FineTuneConfig, stats: Statistics):
+        
+        #TODO: SHOULD LOAD PREVIOUS FINETUNED IF FINETUNE SUPERVISED
+        #TODO: THE MODEL IS  OUTPUTTING ONE SINGLE CHANNEL, 
+        
+        if self.config.optimizer_sup == "sgd":
+            optimizer = torch.optim.SGD(self.model.parameters(), config.lr_sup, momentum=0.9, weight_decay=0.0, nesterov=False)
+        elif self.config.optimizer_sup == "adam":
+            optimizer = torch.optim.Adam(self.model.parameters(), config.lr_sup)
+            
+        if self.config.loss_function_sup == "binary_cross_entropy":
+            criterion = nn.BCELoss() # #model outputs sigmoid so no use of BCEwithLogits
+        elif self.config.loss_function_sup == "dice":
+            criterion = DiceLoss.dice_loss
+        elif self.config.loss_function_sup == "mix_dice_bce":
+            #https://discuss.pytorch.org/t/dice-loss-cross-entropy/53194
+            raise NotImplementedError
+            
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=int(self.config.patience_sup * 0.8), gamma=0.5)
+        best_loss = 10000000000
+        num_epoch_no_improvement = 0
+
+        for epoch in range(self.config.nb_epoch_sup):
+            
+            stats.training_losses_sup = [] 
+            stats.validation_losses_sup = []
+            scheduler.step(epoch)
+            self.model.train()
+            x, iteration = 0 , 0
+            while x != None: # go through all examples
+                x, y = dataset.get_train(self.config.batch_size_sup)
+                x, y = x.float().to(self.device), y.float().to(self.device)
+                pred = self.model(x)
+                loss = criterion(pred, y) 
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                stats.training_losses_sup.append(loss.item())
+
+                if (iteration + 1) % 5 == 0:
+                    print('Epoch [{}/{}], iteration {}, Loss: {:.6f}'.format(epoch + 1, self.config.nb_epoch_sup, iteration + 1, np.average(stats.training_losses_sup)))
+                    sys.stdout.flush()
+                iteration += 1
+
+            with torch.no_grad():
+                self.model.eval()
+                x = 0
+                while x != None:
+                    x, y = dataset.get_val(self.config.batch_size_sup)
+                    x, y = x.float().to(self.device), y.float().to(self.device)
+                    pred = self.model(x)
+                    loss = criterion(pred,y)
+                    stats.validation_losses_sup.append(loss.item())
+                
+            avg_training_loss_of_epoch = np.average(stats.training_losses_sup)      
+            avg_validation_loss_of_epoch = np.average(stats.validation_losses_sup)
+            #logging
+            
+            stats.avg_training_loss_per_epoch_sup.append(avg_training_loss_of_epoch)
+            stats.avg_validation_loss_per_epoch_sup.append(avg_validation_loss_of_epoch)
+            stats.iterations_sup.append(iteration)
+        
+
+            print("Epoch {}, validation loss is {:.4f}, training loss is {:.4f}".format(epoch+1,avg_validation_loss_of_epoch,avg_training_loss_of_epoch))
+            if avg_validation_loss_of_epoch < best_loss:
+                print("Validation loss decreases from {:.4f} to {:.4f}".format(best_loss, avg_validation_loss_of_epoch))
+                best_loss = avg_validation_loss_of_epoch
+                num_epoch_no_improvement = 0
+                #save model
+                torch.save({
+                    'epoch': epoch+1,
+                    'state_dict' : self.model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict()
+                },os.path.join(self.config.model_path_save, self.config.finetune_task + ".pt"))
+                print("Saving model ",os.path.join(self.config.model_path_save, self.config.finetune_task + ".pt"))
+            else:
+                print("Validation loss did not decrease from {:.4f}, num_epoch_no_improvement {}".format(best_loss,num_epoch_no_improvement))
+                num_epoch_no_improvement += 1
+            if num_epoch_no_improvement >= self.config.patience_sup:
+                print("Early Stopping")
+                stats.stopped_early = True
+                break
+            sys.stdout.flush()
+        
+        #TODO: ADD TEST LOSS NOW   
+        stats.get_statistics() 
+        
+    def _loadmodel(self, from_weights=True):
+        
+        self.model = UNet3D()
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if from_weights:
+            weight_dir = self.config.weights
+            checkpoint = torch.load(weight_dir, map_location=self.device)
+            state_dict = checkpoint['state_dict']
+            unParalled_state_dict = {}
+            for key in state_dict.keys():
+                unParalled_state_dict[key.replace("module.", "")] = state_dict[key]
+            self.model.load_state_dict(unParalled_state_dict)
+            
+        self.model.to(self.device)
+        self.model = nn.DataParallel(self.model, device_ids = [i for i in range(torch.cuda.device_count())])
+        
+
+        
+if __name__ == "__main__":
+    
+    config = FineTuneConfig(self_supervised=False)
+    dataset = Dataset(data_dir=config.data_dir)
+    stats = Statistics(config, dataset)
--- pytorch/finetune_config.py
+++ pytorch/finetune_config.py
@@ -1,42 +1,50 @@
 import os
 import shutil
 from datetime import datetime
+from utils import make_dir
 
 class FineTuneConfig:
     
-    def __init__(self, self_supervised=False):
-        #perform supersvised task on new dataset before actually finetuning for segmentation
+    def __init__(self, self_supervised: bool):
+        
         self.self_supervised = self_supervised
     
-    # data
-    data_dir = "pytorch/datasets/Task02_Heart/imagesTr/extracted_cubes"
-    weights = "pretrained_weights/Genesis_Chest_CT.pt" #initial weights
-    finetune_task = "TASK02_Heart_MRI"
-    today_str = datetime.today().strftime('%Y-%m-%d-%H') + "h"
-    stats_dir = os.path.join("stats/", finetune_task, today_str)
-    model_path_save = os.path.join("pretrained_weights/finetuning/", finetune_task, today_str)
-    if not os.path.exists(model_path_save):
-        os.makedirs(model_path_save)
         
-    # model finetuning
-    verbose = 1
-    batch_size = 6
-    optimizer = "sgd"
-    nb_epoch = 1000
-    patience = 50
-    lr = 5e-4  
-    self_supervised = False
-    if self_supervised:
-        finetune_task = "With pre self-supervised" + finetune_task
-
-    # image deformation
-    nonlinear_rate = 0.9
-    paint_rate = 0.9
-    outpaint_rate = 0.8
-    inpaint_rate = 1.0 - outpaint_rate
-    local_rate = 0.5
-    flip_rate = 0.4
-    
+        self.data_dir = "pytorch/datasets/Task02_Heart/imagesTr/extracted_cubes"
+        self.weights = "pretrained_weights/Genesis_Chest_CT.pt" #initial weights
+        self.finetune_task = "TASK02_Heart_MRI"
+        today_str = datetime.today().strftime('%Y-%m-%d-%H') + "h"
+        self.stats_dir = os.path.join("stats/", self.finetune_task, today_str)
+        self.model_path_save = os.path.join("pretrained_weights/finetuning/", self.finetune_task, today_str)
+        make_dir(self.model_path_save)
+            
+        # self supervision finetuning (as done for building model genesis) before supervised finetuning
+        if self.self_supervised:
+            self.finetune_task = "With pre self-supervised" + self.finetune_task
+            self.batch_size_ss = 6
+            self.optimizer_ss = "sgd"
+            self.loss_function_ss = "mse"
+            self.nb_epoch_ss = 1000
+            self.patience_ss = 50
+            self.lr_ss = 5e-2  
+            
+            # image deformation for self supervision
+            self.nonlinear_rate = 0.9
+            self.paint_rate = 0.9
+            self.outpaint_rate = 0.8
+            self.inpaint_rate = 1.0 - self.outpaint_rate
+            self.local_rate = 0.5
+            self.flip_rate = 0.4
+                
+        # supervised finetuning
+        self.batch_size_sup = 6
+        self.optimizer_sup = "sgd"
+        self.loss_function_sup = ""
+        self.nb_epoch_sup = 10000
+        self.patience_sup = 50
+        self.lr_sup = 5e-4  
+        #self.threshold =  0.5 #above is considered part of mask
+            
     def display(self):
         """Display Configuration values."""
         print("\nConfigurations:")
@@ -44,3 +52,4 @@ class FineTuneConfig:
             if not a.startswith("__") and not callable(getattr(self, a)):
                 print("{:30} {}".format(a, getattr(self, a)))
         print("\n")
+
