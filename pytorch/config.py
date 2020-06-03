6823e9f7e18f12fef444e855da0d529763aada80
moutan <moutan@localhost.localdomain>
2020-06-03 15:13:47 +0000

    ready genesis replication experiment

--- pytorch/config.py
+++ pytorch/config.py
@@ -1,16 +1,33 @@
 import os
 import shutil
 
+#FILE IS UNTOUCHED
+
+def _get_task_dir(task):
+    # get dir corresponding to next numerical experiment
+    task_dir = task + "/run"
+    experiment_nr = 1
+    while os.path.isdir(os.path.join("runs/", task + "_" + str(experiment_nr) + "/")): #meaning the experiment has not been run
+        experiment_nr += 1
+        return task_dir + "_" + str(experiment_nr) + "/" 
+
 class models_genesis_config:
+    
     model = "Unet3D"
     suffix = "genesis_chest_ct"
     exp_name = model + "-" + suffix
     
+    task = "GENESIS_REPLICATION"
+    task_dir = _get_task_dir(task)
+    stats_dir = os.path.join("stats/", task_dir)
+    model_path_save = os.path.join("pretrained_weights/finetuning/", task_dir)
+    summarywriter_dir = os.path.join("runs/", task_dir)
+    
     # data
-    data = "datasets/luna16_cubes"
-    train_fold=[0]
-    valid_fold=[2]
-    test_fold=[3]
+    data_dir = "/work1/s182312/luna16_extracted_cubes/scale_32"
+    train_fold =[0,1,2,3,4]
+    valid_fold=[5,6]
+    test_fold=[7,8,9]
     hu_min = -1000.0
     hu_max = 1000.0
     scale = 32
@@ -20,16 +37,17 @@ class models_genesis_config:
     nb_class = 1
     
     # model pre-training
+    self_supervised = True
     verbose = 1
-    weights = "pretrained_weights/Genesis_Chest_CT.pt"
-    batch_size = 6
-    optimizer = "sgd"
+    weights = None
+    batch_size_ss = 6
+    optimizer_ss = "sgd"
     workers = 10
     max_queue_size = workers * 4
     save_samples = "png"
-    nb_epoch = 10000
-    patience = 50
-    lr = 1
+    nb_epoch_ss = 10000
+    patience_Ss = 50
+    lr_ss = 1
 
     # image deformation
     nonlinear_rate = 0.9
@@ -39,6 +57,15 @@ class models_genesis_config:
     local_rate = 0.5
     flip_rate = 0.4
     
+    #in here just to conform with statistics module
+    batch_size_sup = None
+    optimizer_sup = None
+    loss_function_sup = None #binary_cross_entropy
+    nb_epoch_sup = None
+    patience_sup = None
+    lr_sup = None
+    threshold =  None #above is considered part of mask
+
     # logs
     model_path = "pretrained_weights"
     if not os.path.exists(model_path):
---
+++ pytorch/main.py
@@ -0,0 +1,26 @@
+from finetune_config import FineTuneConfig
+from config import models_genesis_config
+from dataset import Dataset
+from stats import Statistics
+from finetune import Trainer
+
+# script to run experiments
+
+# result replication
+
+config = models_genesis_config()
+dataset = Dataset(config.data_dir, train_val_test=(0.8, 0.2, 0)) # train_val_test is non relevant as will ve overwritten after
+dataset.x_train_filenames = ["bat_32_s_64x64x32_" + str(i) + ".npy" for i in config.train_fold]
+dataset.x_val_filenames = ["bat_32_s_64x64x32_" + str(i) + ".npy" for i in config.valid_fold]
+dataset.x_test_filenames = ["bat_32_s_64x64x32_" + str(i) + ".npy" for i in config.test_fold] #Dont know in what sense they use this for
+stats = Statistics(config, dataset)
+trainer_mg_replication = Trainer(config, dataset,stats)
+trainer_mg_replication.train_from_scratch_model_model_genesis_exact_replication()
+
+
+
+
+
+
+
+
