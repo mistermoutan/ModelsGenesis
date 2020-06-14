ebaf0d86b479cbeb84335fe2f60e95945a354db6
moutan <moutan@localhost.localdomain>
2020-06-14 22:45:08 +0000

    tweak stats

--- pytorch/stats.py
+++ pytorch/stats.py
@@ -7,9 +7,10 @@ import os
 from utils import make_dir
 from collections import OrderedDict
 import json
-
 from finetune_config import FineTuneConfig
+from config import models_genesis_config
 from dataset import Dataset
+from datasets import Datasets
 
 """
 Class to benchmark performance of methods
@@ -21,13 +22,16 @@ HOW TO USE:
 
 class Statistics:
     
-    def __init__(self, config: FineTuneConfig, dataset: Dataset):
+    def __init__(self, config: FineTuneConfig or models_genesis_config, dataset: Dataset or Datasets):
         """
         save_directory: directory where you want to save the stats
         """
         
         self.config = config
         self.dataset = dataset
+        if isinstance(self.dataset, Datasets):
+            self.multiple_datasets = True 
+
         self.start_time = time.time()
         self.save_directory = config.stats_dir
         make_dir(self.save_directory)
@@ -64,13 +68,18 @@ class Statistics:
         self.stats = OrderedDict()
         self.stats["date"] = time.strftime("%c")
         self.stats["training_time"] = str(datetime.timedelta(seconds=self.end_time - self.start_time))
-        self.stats["tr_val_ts_split"] = self.dataset.tr_val_ts_split
-        self.task = self.config.task
-        self.stats["task"] = self.task
-
-        self.stats["training_files"] = self.dataset.x_train_filenames
-        self.stats["validation_files"] = self.dataset.x_val_filenames
-        self.stats["test_files"] = self.dataset.x_test_filenames
+        self.stats["task"] = self.config.task
+        
+        self.stats["dataset_dir"] = self.config.data_dir # will give us names of datasets used
+        if not self.multiple_datasets:
+            self.stats["tr_val_ts_split"] = self.dataset.tr_val_ts_split
+        else:
+            self.stats["tr_val_ts_split"] = self.dataset[0].tr_val_ts_split 
+            
+        #self.stats["training_files"] = self.dataset.x_train_filenames
+        #self.stats["validation_files"] = self.dataset.x_val_filenames
+        #self.stats["test_files"] = self.dataset.x_test_filenames
+        
         self.stats["self_supervised_used"] = True if self.config.self_supervised else False
 
         if self.config.self_supervised:    
@@ -79,15 +88,18 @@ class Statistics:
             self.stats["initial_lr_ss"] = self.config.lr_ss
             self.stats["loss_function_ss"] = self.config.loss_function_ss
             self.stats["optimizer_ss"] = self.config.optimizer_ss
+            self.stats["scheduler_ss"] = self.config.scheduler_ss
             self.stats["stopped_early_ss"] = self.stopped_early_ss
             self.stats["iterations_ss"] = set(self.iterations_ss)
             self.stats["comment_ss"] = self.comment_ss
+            
         
         self.stats["epochs_sup"] = self.config.nb_epoch_sup
         self.stats["batch_size_sup"] = self.config.batch_size_sup
         self.stats["initial_lr_sup"] = self.config.lr_sup
         self.stats["loss_function_sup"] = self.config.loss_function_sup 
         self.stats["optimizer_sup"] = self.config.optimizer_sup
+        self.stats["scheduler_sup"] = self.config.scheduler_sup
         self.stats["stopped_early_sup"] = self.stopped_early_sup
         self.stats["iterations_sup"] = set(self.iterations_sup)
         self.stats["comment_sup"] = self.comment_sup
@@ -98,9 +110,9 @@ class Statistics:
         """ saves arrays in dict, may be relevant for future comparisons"""
 
         dict_of_arrays = OrderedDict()
-        dict_of_arrays["training_files"] = self.dataset.x_train_filenames
-        dict_of_arrays["validation_files"] = self.dataset.x_val_filenames
-        dict_of_arrays["test_files"] = self.dataset.x_test_filenames
+        #dict_of_arrays["training_files"] = self.dataset.x_train_filenames
+        #dict_of_arrays["validation_files"] = self.dataset.x_val_filenames
+        #ict_of_arrays["test_files"] = self.dataset.x_test_filenames
         
         if self.config.self_supervised:
             dict_of_arrays["avg_training_loss_per_epoch_ss"] = self.avg_training_loss_per_epoch_ss
@@ -145,12 +157,12 @@ class Statistics:
     def save_log_book (self, save_json=True):
         
         if save_json:
-            with open(self.save_directory + "log.json",'w') as logbook:
+            with open(self.save_directory + "log.json", 'w') as logbook:
                 json.dump(self.stats, logbook)
         else:
-            with open(self.save_directory + "log.txt",'w') as logbook:
+            with open(self.save_directory + "log.txt", 'w') as logbook:
                 for key,value in self.stats.items():
-                    logbook.write("{0} : {1}  \n " .format(key,value))
+                    logbook.write("{0} : {1}  \n " .format(key, value))
     
     @staticmethod
     def write_pickle (f, path, fname):
