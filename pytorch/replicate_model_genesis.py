70c4705011ba7615ff0b75169ace067dba282e41
moutan <moutan@localhost.localdomain>
2020-06-24 17:40:31 +0000

    add replicate model genesis cause no command line tool

---
+++ pytorch/replicate_model_genesis.py
@@ -0,0 +1,30 @@
+from finetune_config import FineTuneConfig
+from config import models_genesis_config
+from dataset import Dataset
+from finetune import Trainer
+from utils import make_dir
+
+
+def replication_of_results_pretrain():
+
+    #CUBE LOGIC
+    # result replication, try to get the pretrained model
+    config = models_genesis_config()
+    config.display()
+    x_train_filenames = ["bat_32_s_64x64x32_" + str(i) + ".npy" for i in config.train_fold]
+    x_val_filenames = ["bat_32_s_64x64x32_" + str(i) + ".npy" for i in config.valid_fold]
+    x_test_filenames = ["bat_32_s_64x64x32_" + str(i) + ".npy" for i in config.test_fold] #Dont know in what sense they use this for
+    files = [x_train_filenames, x_val_filenames, x_test_filenames]
+    dataset = Dataset(config.data_dir, train_val_test=(0.8, 0.2, 0), file_names=files) # train_val_test is non relevant as is overwritten by files
+
+    trainer_mg_replication = Trainer(config, dataset)
+    trainer_mg_replication.load_model(from_scratch=True)
+    trainer_mg_replication.finetune_self_supervised()
+    trainer_mg_replication.add_hparams_to_writer()
+    trainer_mg_replication.get_stats()
+    
+if if __name__ == "__main__":
+    
+    print("STARTING REPLICATION OF RESULTS EXPERIMENT")
+    replication_of_results_pretrain()
+    print("FINISHED REPLICATION OF RESULTS EXPERIMENT")
