0c797f67e4dde75bbbf744c925a015eb2f684c7d
moutan <moutan@localhost.localdomain>
2020-07-01 15:05:48 +0000

    check timedelta of pair generation

--- pytorch/finetune.py
+++ pytorch/finetune.py
@@ -78,7 +78,10 @@ class Trainer:
             while True:  # go through all examples
                 x, _ = self.dataset.get_train(batch_size=self.config.batch_size_ss, return_tensor=False)
                 if x is None: break
+                transform_start_time = time.time()
                 x_transform, y = generate_pair(x, self.config.batch_size_ss, self.config, make_tensors=True)
+                transform_timedelta = timedelta(seconds= time.time() - self.transform_start_time)
+                print("TOOK {} seconds to generate pair".format(str(transform_timedelta // 60 % 60)))
                 x_transform, y = x_transform.float().to(self.device), y.float().to(self.device)
                 pred = self.model(x_transform) 
                 #print("INFERENCE SUCCESSFUL")
