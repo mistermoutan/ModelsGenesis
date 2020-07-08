103b22dec874eb7e60d3500c2b6780cbbb4e0c71
moutan <moutan@localhost.localdomain>
2020-07-08 15:05:34 +0000

    update dataset pytorch

--- pytorch/dataset_pytorch.py
+++ pytorch/dataset_pytorch.py
@@ -9,8 +9,7 @@ from image_transformations import generate_pair
 
 
 class DatasetPytorch(Dataset):
-    
-    def __init__(self, dataset, config, type_:str, apply_mg_transforms:bool):
+    def __init__(self, dataset, config, type_: str, apply_mg_transforms: bool):
         """[summary]
 
         Args:
@@ -19,7 +18,7 @@ class DatasetPytorch(Dataset):
             type_ (str): "train", "val" or "ts"
             apply_mg_transforms (bool) True for self supervision
         """
-        
+
         self.dataset = dataset
         self.config = config
         self.type = type_
@@ -27,9 +26,9 @@ class DatasetPytorch(Dataset):
         self.nr_samples_train = None
         self.nr_samples_val = None
         self.nr_samples_test = None
-        
+
     def __len__(self):
-        
+
         if self.type == "train":
             if self.nr_samples_train is None:
                 np_array_names = self.dataset.x_train_filenames_original
@@ -40,7 +39,7 @@ class DatasetPytorch(Dataset):
                 return self.nr_samples_train
             else:
                 return self.nr_samples_train
-            
+
         if self.type == "val":
             if self.nr_samples_val is None:
                 np_array_names = self.dataset.x_val_filenames_original
@@ -51,7 +50,7 @@ class DatasetPytorch(Dataset):
                 return self.nr_samples_val
             else:
                 return self.nr_samples_val
-            
+
         if self.type == "ts":
             if self.nr_samples_test is None:
                 np_array_names = self.dataset.x_test_filenames_original
@@ -62,45 +61,44 @@ class DatasetPytorch(Dataset):
                 return self.nr_samples_test
             else:
                 return self.nr_samples_test
-    
+
     def __getitem__(self, idx):
-        
+
         if self.type == "train":
-            x, y = self.dataset.get_train(batch_size=1, return_tensor=True)
+
+            x, y = self.dataset.get_train(batch_size=1, return_tensor=False) if self.apply_mg_transforms else self.dataset.get_train(batch_size=1, return_tensor=True)
             if x is not None:
                 if self.apply_mg_transforms:
                     x_transform, y = generate_pair(x, 1, self.config, make_tensors=True)
                     return (x_transform, y)
-                return (x,y)
-           
+                return (x, y)
+
         if self.type == "val":
-            x, y = self.dataset.get_val(batch_size=1, return_tensor=True)
+            x, y = self.dataset.get_val(batch_size=1, return_tensor=False) if self.apply_mg_transforms else self.dataset.get_val(batch_size=1, return_tensor=True)
             if x is not None:
                 if self.apply_mg_transforms:
                     x_transform, y = generate_pair(x, 1, self.config, make_tensors=True)
                     return (x_transform, y)
                 return (x, y)
-        
+
         if self.type == "ts":
-            x, y = self.dataset.get_test(batch_size=1, return_tensor=True)
+            x, y = self.dataset.get_test(batch_size=1, return_tensor=False) if self.apply_mg_transforms else self.dataset.get_test(batch_size=1, return_tensor=True)
             if x is not None:
                 if self.apply_mg_transforms:
                     x_transform, y = generate_pair(x, 1, self.config, make_tensors=True)
                     return (x_transform, y)
                 return (x, y)
-        
+
     @staticmethod
     def custom_collate(batch):
         dims = batch[0][0].shape
         x_tensor = torch.zeros(len(batch), 1, dims[2], dims[3], dims[4])
         y_tensor = torch.zeros(len(batch), 1, dims[2], dims[3], dims[4])
-        for idx, (x, y) in enumerate(batch): # y can be none in ss case
+        for idx, (x, y) in enumerate(batch):  # y can be none in ss case
             x_tensor[idx] = x
-            if y is None: y_tensor = None
-            else: y_tensor[idx] = y
-            
-        return (x,y)
-            
-            
-            
-                
+            if y is None:
+                y_tensor = None
+            else:
+                y_tensor[idx] = y
+
+        return (x, y)
