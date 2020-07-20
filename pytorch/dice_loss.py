1b53cd3fa31479b546329978215b6c5f442382ff
moutan <moutan@protonmail.com>
2020-07-20 16:37:18 +0200

    .

--- pytorch/dice_loss.py
+++ pytorch/dice_loss.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 
 
 class DiceLoss:
-    @staticmethod
 
     # TODO: dice needs threshold here no, actually no, so the network after reaching 0.5 has no incentive to become "more certain"
 
+    @staticmethod
     def dice_loss(pred, target, smooth=0, eps=1e-7, return_loss=True):
         """
         pred: tensor with first dimension as batch
@@ -49,6 +49,27 @@ if __name__ == "__main__":
             # print(a)
             # print(b)
             pass
+
+
+""" def compute_dice_coefficient(mask_gt, mask_pred):
+  Compute soerensen-dice coefficient.
+
+  compute the soerensen-dice coefficient between the ground truth mask `mask_gt`
+  and the predicted mask `mask_pred`. 
+  
+  Args:
+    mask_gt: 3-dim Numpy array of type bool. The ground truth mask.
+    mask_pred: 3-dim Numpy array of type bool. The predicted mask.
+
+  Returns:
+    the dice coeffcient as float. If both masks are empty, the result is NaN
+  
+  volume_sum = mask_gt.sum() + mask_pred.sum()
+  if volume_sum == 0:
+    return np.NaN
+  volume_intersect = (mask_gt & mask_pred).sum()
+  return 2*volume_intersect / volume_sum """
+
 
 r""" 
 
