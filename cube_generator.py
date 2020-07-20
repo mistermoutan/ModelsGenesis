f8fb0287216d5a6ebbc65a62378e02b8f9950e2c
moutan <moutan@protonmail.com>
2020-07-20 15:45:03 +0200

    automatic reduce cube sizes if input is smaller than intended

--- cube_generator.py
+++ cube_generator.py
@@ -252,7 +252,27 @@ def get_self_learning_data(config):
 
         itk_img = sitk.ReadImage(os.path.join(config.DATA_DIR, volume))
         img_array = sitk.GetArrayFromImage(itk_img)
+        if img_array.shape == 4:
+            raise NotImplementedError
+            # img_array = img_array[]
         img_array = img_array.transpose(2, 1, 0)
+
+        # handle small cubes dataset
+        while img.array.shape[0] < config.input_rows:
+            if config.input_rows == 16:
+                print("DATAST DIMENSIONS TOO SMALL")
+            config.input_rows /= 2
+        while img.array.shape[1] < config.input_cols:
+            if config.input_cols == 16:
+                print("DATAST DIMENSIONS TOO SMALL")
+            config.input_cols /= 2
+        while img.array.shape[2] < config.input_deps:
+            if config.input_deps == 16:
+                print("DATAST DIMENSIONS TOO SMALL")
+            config.input_deps /= 2
+
+        config.crop_rows = config.input_rows
+        config.crop_cols = config.input_cols
 
         if not config.target_dir:
             x = infinite_generator_from_one_volume(config, img_array)
