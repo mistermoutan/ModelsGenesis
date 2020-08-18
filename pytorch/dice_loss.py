fix dice calculation for unbatched, "unchanneled" cubes

pytorch/dice_loss.py
@@ -21,9 +21,13 @@ class DiceLoss:
         if not pred.device == target.device:
             raise ValueError("input and target must be in the same device. Got: {} and {}".format(pred.device, target.device))
 
-        # have to use contiguous since they may from a torch.view op
-        iflat = pred[:, 0].contiguous().view(-1)  # one channel only N x 1 x H x D X W -> N x H x D x W
-        tflat = target[:, 0].contiguous().view(-1)
+        if len(pred.shape) != 3:
+            # works for both  (N,C,x,y) and (N,C,x,y,z)
+            iflat = pred[:, 0].contiguous().view(-1)  # one channel only N x 1 x H x D X W -> N x H x D x W
+            tflat = target[:, 0].contiguous().view(-1)
+        else:
+            iflat = pred.contiguous().view(-1)  # comes as (x,y,z) so flatten everything
+            tflat = target.contiguous().view(-1)
 
         intersection = torch.sum(iflat * tflat)
         A_sum_sq = torch.sum(iflat * iflat)