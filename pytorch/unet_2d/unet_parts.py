b53b5b94ccce518a406e905df4fdddb176948225
moutan <moutan@protonmail.com>
2020-08-21 10:35:47 +0200

    fix skip connections padding

--- pytorch/unet_2d/unet_parts.py
+++ pytorch/unet_2d/unet_parts.py
@@ -52,14 +52,20 @@ class Up(nn.Module):
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        if len(x1.shape) == 5:  # 3D because ACS
+            diffX = x2.size()[2] - x1.size()[2]
+            diffY = x2.size()[3] - x1.size()[3]
+            diffZ = x2.size()[4] - x1.size()[4]
+            pad_tuple = tuple([diffZ // 2, diffZ - diffZ // 2, diffY // 2, diffY - diffY // 2, diffZ // 2, diffX - diffX // 2, 0, 0, 0, 0])
+            x1 = F.pad(x1, pad_tuple)
+
+        elif len(x1.shape) == 4:  # input is BCHW
+            diffX = x2.size()[2] - x1.size()[2]
+            diffY = x2.size()[3] - x1.size()[3]
+            pad_tuple = tuple([diffY // 2, diffY - diffY // 2, diffX // 2, diffX - diffX // 2, 0, 0, 0, 0])
+            x1 = F.pad(x1, pad_tuple)
+
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
