db27208473af73a59701021d826dc0b6d93175d3
moutan <moutan@protonmail.com>
2020-08-27 15:14:27 +0200

    add test max dim cluster can handle

--- pytorch/test_memory_inference.py
+++ pytorch/test_memory_inference.py
@@ -2,8 +2,9 @@ import torch
 import SimpleITK as sitk
 import numpy as np
 from time import time
-
+from utils import *
 from unet3d import UNet3D
+from random import choice
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 unet3d = UNet3D()
@@ -17,8 +18,38 @@ img_array = np.expand_dims(img_array, (0, 1))
 img_array = torch.Tensor(img_array)
 img_array = img_array.float().to(device)
 img_array = img_array.contiguous()
+img_array, _ = pad_if_necessary_one_array(img_array, return_pad_tuple=False)
 
-with torch.no_grad():
-    unet3d.eval()
-    pred = unet3d(img_array)
-    print(pred.shape)
+
+def do_inference(array):
+
+    with torch.no_grad():
+        unet3d.eval()
+        try:
+            pred = unet3d(array)
+            return pred.shape
+        except:
+            return None
+
+
+def clip_array(array):
+    possible = [idx for idx, i in enumerate(array.shape) if i > 16]
+    dim = choice(possible)
+    if dim == 2:
+        array = array[:, :, 15:, :, :]
+    elif dim == 3:
+        array = array[:, :, :, 15:, :]
+    elif dim == 4:
+        array = array[:, :, :, :, 15:]
+    else:
+        raise ValueError
+
+    return array
+
+
+while do_inference(img_array) is None:
+    img_array = clip_array(img_array)
+
+print("WORKED: ", img_array.shape)
+s = do_inference(img_array)
+print(s)
