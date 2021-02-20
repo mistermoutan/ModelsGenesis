7f5b7a84a39c57314bf6c6d524295e792a5924c8
mistermoutan <moutan@protonmail.com>
2021-02-20 21:17:08 +0000

    add save segmentation examples mini cubes

--- pytorch/evaluate.py
+++ pytorch/evaluate.py
@@ -265,12 +265,199 @@ class Tester:
             return train_minicubes_filenames
 
         return train_minicubes_filenames[:k]
-            state_dict = checkpoint["state_dict"]
 
-        unParalled_state_dict = {}
-        for key in state_dict.keys():
-            unParalled_state_dict[key.replace("module.", "")] = state_dict[key]
-        self.model.load_state_dict(unParalled_state_dict)
+    def save_segmentation_examples(self, nr_cubes=5):
+
+        # for mini cubes
+
+        cubes_to_use = []
+        cubes_to_use.extend(self.sample_k_mini_cubes_which_were_used_for_testing(nr_cubes))
+        cubes_to_use.extend(self.sample_k_mini_cubes_which_were_used_for_training(nr_cubes))
+        cubes_to_use_path = [os.path.join(self.dataset.x_data_dir, i) for i in cubes_to_use]
+        label_cubes_of_cubes_to_use_path = []
+        for cube_name in cubes_to_use:
+            if os.path.isfile(os.path.join(self.dataset.y_data_dir, cube_name)):
+                label_cubes_of_cubes_to_use_path.append(os.path.join(self.dataset.y_data_dir, cube_name))
+            elif os.path.isfile(os.path.join(self.dataset.y_data_dir, cube_name[:-4] + "_target.npy")):
+                label_cubes_of_cubes_to_use_path.append(os.path.join(self.dataset.y_data_dir, cube_name))
+            else:
+                raise FileNotFoundError
+
+        self.two_dim = True if self.config.model.lower() == "unet_2d" else False
+
+        for cube_idx, cube_path in enumerate(cubes_to_use_path):
+            np_array = self._load_cube_to_np_array(cube_path)  # (N,x,y,z)
+            np_array = (
+                np_array[:3] if self.dataset_name != "lidc" else np_array[:10]
+            )  # use number of samples lidc is different as training are all in the same file
+            self.original_cube_dimensions = np_array.shape[1:]
+
+            for sample_idx in np_array.shape[0]:
+                mini_cube_tensor = torch.Tensor(np_array[sample_idx])  # (N,x,y,z) -> (x,y,z)
+                mini_cube_tensor = torch.unsqueeze(mini_cube_tensor, 0)  # (x,y,z) -> (1,x,y,z)
+                mini_cube_tensor = torch.unsqueeze(mini_cube_tensor, 0)  # (1,x,y,z) -> (1,1,x,y,z)
+                with torch.no_grad():
+                    self.trainer.model.eval()
+                    if self.two_dim is False:
+                        if self.config.model.lower() in ("vnet_mg", "unet_3d", "unet_acs"):
+                            mini_cube_tensor, pad_tuple = pad_if_necessary_one_array(mini_cube_tensor, return_pad_tuple=True)
+                            pred = self.model(mini_cube_tensor)
+                            pred.to("cpu")
+                            pred = FullCubeSegmentator._unpad_3d_array(pred, pad_tuple)
+                            pred = torch.squeeze(pred, dim=0)  # (1, 1, C,H,W) -> (1,C,H,W)
+                            pred = torch.squeeze(pred, dim=0)
+                            pred_mask_mini_cube = pred  # self._make_pred_mask_from_pred(pred)
+                        elif "fcn_resnet" in self.config.model.lower():
+                            x = torch.cat((mini_cube_tensor, mini_cube_tensor, mini_cube_tensor), dim=1)
+                            if 86 in x.shape:
+                                continue
+                            pred_fcn = self.model(x)  # (1,2,C,H,W), channel 1 is target and channel 0 is background
+                            pred_mask_mini_cube = pred_fcn[:, 1]  #  or pred[:, 1] - pred[:, 0] ; (1,1,C,H,W)
+                            pred_mask_mini_cube = torch.squeeze(pred_mask_mini_cube, dim=0)  # (1, 1, C,H,W) -> (1,C,H,W)
+                            pred_mask_mini_cube = torch.squeeze(pred_mask_mini_cube, dim=0)
+                    else:
+                        pred_mask_mini_cube = torch.zeros(self.original_cube_dimensions)
+                        for z_idx in range(mini_cube_tensor.size()[-1]):
+                            tensor_slice = mini_cube_tensor[..., z_idx]  # SLICE : (1,1,C,H,W) -> (1,1,C,H)
+                            assert tensor_slice.shape == (1, 1, self.original_cube_dimensions[0], self.original_cube_dimensions[1])
+                            pred = self.trainer.model(tensor_slice)
+                            pred = torch.squeeze(pred, dim=0)  # (1, 1, C,H) -> (1,C,H)
+                            pred = torch.squeeze(pred, dim=0)  # (1,C,H) -> (C,H)
+                            pred_mask_mini_cube[..., z_idx] = pred
+
+                if cube_idx < nr_cubes:
+                    save_dir = os.path.join(
+                        self.save_dir,
+                        self.dataset_name,
+                        "testing_examples_mini/",
+                        cubes_to_use[cube_idx][:-4] + "_sample{}".format(sample_idx),
+                    )
+                else:
+                    save_dir = os.path.join(
+                        self.save_dir,
+                        self.dataset_name,
+                        "training_examples_mini/",
+                        cubes_to_use[cube_idx][:-4] + "_sample{}".format(sample_idx),
+                    )
+
+                make_dir(save_dir)
+
+                pred_mask_mini_cube = pred_mask_mini_cube.cpu()  # logits mask
+                if "fcn_resnet" not in self.config.model.lower():
+                    pred_mask_mini_cube_binary = self._make_pred_mask_from_pred(pred_mask_mini_cube)  # binary mask
+                else:
+                    # we want to use argmax in the dual channel output when fcn resnet 18
+                    # pred_mask_mini_cube is 1 channel and  "hacked it" in the case of fcn resnet 18 case
+                    pred_mask_mini_cube_binary = self._make_pred_mask_from_pred(pred_fcn)
+                    pred_mask_mini_cube_binary = torch.squeeze(pred_mask_mini_cube_binary, dim=0)
+                    pred_mask_mini_cube_binary = torch.squeeze(pred_mask_mini_cube_binary, dim=0)
+                    assert len(pred_mask_mini_cube_binary.shape) == 3
+
+                # save nii's
+                nifty_img = nibabel.Nifti1Image(np.array(pred_mask_mini_cube).astype(np.float32), np.eye(4))
+                nibabel.save(
+                    nifty_img, os.path.join(save_dir, cubes_to_use[cube_idx][:-4] + "_sample{}_logits_mask.nii.gz".format(sample_idx))
+                )
+
+                nifty_img = nibabel.Nifti1Image(np.array(pred_mask_mini_cube_binary).astype(np.float32), np.eye(4))
+                nibabel.save(
+                    nifty_img, os.path.join(save_dir, cubes_to_use[cube_idx][:-4] + "_sample{}_binary_mask.nii.gz".format(sample_idx))
+                )
+
+                # save .nii.gz of cube if is npy original full cube file
+                if ".npy" in cube_path:
+                    nifty_img = nibabel.Nifti1Image(np_array[sample_idx].astype(np.float32), np.eye(4))
+                    nibabel.save(nifty_img, os.path.join(save_dir, cubes_to_use[cube_idx][:-4] + "_mini_cube{}.nii.gz".format(sample_idx)))
+
+                # self.save_3d_plot(np.array(pred_mask_full_cube), os.path.join(save_dir, "{}_plt3d.png".format(cubes_to_use[idx]))))
+
+                label_tensor_of_cube = torch.Tensor(self._load_cube_to_np_array(label_cubes_of_cubes_to_use_path[cube_idx]))[sample_idx]
+                label_tensor_of_cube_masked = np.array(label_tensor_of_cube)
+                label_tensor_of_cube_masked = np.ma.masked_where(
+                    label_tensor_of_cube_masked < 0.5, label_tensor_of_cube_masked
+                )  # it's binary anyway
+
+                pred_mask_mini_cube_binary_masked = np.array(pred_mask_mini_cube_binary)
+                pred_mask_mini_cube_binary_masked = np.ma.masked_where(
+                    pred_mask_mini_cube_binary_masked < 0.5, pred_mask_mini_cube_binary_masked
+                )  # it's binary anyway
+
+                pred_mask_mini_cube_logits_masked = np.array(pred_mask_mini_cube)
+                pred_mask_mini_cube_logits_masked = np.ma.masked_where(
+                    pred_mask_mini_cube_logits_masked < 0.3, pred_mask_mini_cube_logits_masked
+                )
+
+                make_dir(os.path.join(save_dir, "slices_sample{}/".format(sample_idx)))
+
+                for z_idx in range(pred_mask_mini_cube.shape[-1]):
+
+                    # binary
+                    fig = plt.figure(figsize=(10, 5))
+                    plt.imshow(np_array[sample_idx][:, :, z_idx], cmap=cm.Greys_r)
+                    plt.imshow(pred_mask_mini_cube_binary_masked[:, :, z_idx], cmap="Accent")
+                    plt.axis("off")
+                    fig.savefig(
+                        os.path.join(save_dir, "slices_sample{}/".format(sample_idx), "slice_{}_binary.jpg".format(z_idx + 1)),
+                        bbox_inches="tight",
+                        dpi=150,
+                    )
+                    plt.close(fig=fig)
+
+                    # logits
+                    fig = plt.figure(figsize=(10, 5))
+                    plt.imshow(np_array[sample_idx][:, :, z_idx], cmap=cm.Greys_r)
+                    plt.imshow(pred_mask_mini_cube_logits_masked[:, :, z_idx], cmap="Blues", alpha=0.5)
+                    plt.axis("off")
+                    fig.savefig(
+                        os.path.join(save_dir, "slices_sample{}/".format(sample_idx), "slice_{}_logits.jpg".format(z_idx + 1)),
+                        bbox_inches="tight",
+                        dpi=150,
+                    )
+                    plt.close(fig=fig)
+
+                    # dist of logits histogram
+                    distribution_logits = np.array(pred_mask_mini_cube[:, :, z_idx].contiguous().view(-1))
+                    fig = plt.figure(figsize=(10, 5))
+                    plt.hist(distribution_logits, bins=np.arange(min(distribution_logits), max(distribution_logits) + 0.05, 0.05))
+                    fig.savefig(
+                        os.path.join(save_dir, "slices_sample{}/".format(sample_idx), "slice_{}_logits_histogram.jpg".format(z_idx + 1)),
+                        bbox_inches="tight",
+                        dpi=150,
+                    )
+                    plt.close(fig=fig)
+
+                    # save ground truth as wel, overlayed on original
+                    fig = plt.figure(figsize=(10, 5))
+                    plt.imshow(np_array[sample_idx][:, :, z_idx], cmap=cm.Greys_r)
+                    plt.imshow(label_tensor_of_cube_masked[:, :, z_idx], cmap="jet")
+                    plt.axis("off")
+                    fig.savefig(
+                        os.path.join(save_dir, "slices_sample{}/".format(sample_idx), "slice_{}_gt.jpg".format(z_idx + 1)),
+                        bbox_inches="tight",
+                        dpi=150,
+                    )
+                    plt.close(fig=fig)
+
+                dice_score_soft = float(DiceLoss.dice_loss(pred_mask_mini_cube, label_tensor_of_cube, return_loss=False))
+                dice_score_binary = float(DiceLoss.dice_loss(pred_mask_mini_cube_binary, label_tensor_of_cube, return_loss=False))
+                x_flat = pred_mask_mini_cube_binary.contiguous().view(-1)
+                y_flat = label_tensor_of_cube.contiguous().view(-1)
+                x_flat = x_flat.cpu()
+                y_flat = y_flat.cpu()
+                jaccard_scr = jaccard_score(y_flat, x_flat)
+                metrics = {"dice_logits": dice_score_soft, "dice_binary": dice_score_binary, "jaccard": jaccard_scr}
+                # print(dice)
+                with open(os.path.join(save_dir, "dice.json"), "w") as f:
+                    json.dump(metrics, f)
+
+    def _load_cube_to_np_array(self, cube_path):
+
+        if ".npy" in cube_path:
+            img_array = np.load(cube_path)
+        else:
+            # mini cubes were all made .npy in preprocessing
+            raise ValueError
+        return img_array
 
     def _make_pred_mask_from_pred(self, pred, threshold=0.5, print_=True):
         if pred.shape[1] == 1:
@@ -301,8 +488,8 @@ class Tester:
 
 if __name__ == "__main__":
 
-    config = load_object("objects/FROM_SCRATCH_cellari_heart_UNET_3D/only_supervised/run_2/config.pkl")
-    dataset = load_object("objects/FROM_SCRATCH_cellari_heart_UNET_3D/only_supervised/run_2/dataset.pkl")
+    config = load_object("objects/FROM_SCRATCH_fake_small_UNET_ACS/only_supervised/run_5/config.pkl")
+    dataset = load_object("objects/FROM_SCRATCH_fake_small_UNET_ACS/only_supervised/run_5/dataset.pkl")
     t = Tester(config, dataset, test_all=False)
     t.test_segmentation()
 
