dea4b8261bd4c9c049e3ace8aad900165d8c6638
mistermoutan <moutan@protonmail.com>
2021-02-27 12:46:01 +0000

    up kernel size to match

--- pytorch/full_cube_segmentation.py
+++ pytorch/full_cube_segmentation.py
@@ -355,7 +355,7 @@ class FullCubeSegmentator:
         for cube_idx, cube_path in enumerate(cubes_to_use_path):
             np_array = self._load_cube_to_np_array(cube_path)  # (x,y,z)
             self.original_cube_dimensions = np_array.shape
-            if sum([i for i in np_array.shape]) > 500 and self.two_dim is False:
+            if sum([i for i in np_array.shape]) > 600 and self.two_dim is False:
                 inference_full_image = False
 
             if self.dataset_name.lower() in ("task04_sup", "task01_sup", "cellari_heart_sup_10_192", "cellari_heart_sup"):
